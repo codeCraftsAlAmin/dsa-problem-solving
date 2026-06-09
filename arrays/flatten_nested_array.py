53c139933fc2dc7db10a07f5b2c64abc26f06296
Code Crafts <codecraftsman12@gmail.com>
2026-06-09 20:00:20 +0600

    feat: solved flatten_nested_array

---
+++ arrays/flatten_nested_array.py
@@ -0,0 +1,30 @@
+# ============================================
+# Problem: Array
+# Interview: #8
+# Difficulty: Easy
+# ============================================
+#
+# Problem Statement:
+# Flatten Nested Array.
+# Input: flattenArray([1, [2, 3], [4, 5], 6])
+# Output: ([1, 2, 3, 4, 5, 6])
+#
+# Time Complexity: O(n)
+# Space Complexity: O(n)
+# ============================================
+
+arr = [1, [2, 3], [4, 5], 6]
+
+
+def flattenArray(arr):
+    store = []
+    for num in arr:
+        if isinstance(num, list):
+            store.extend(num)
+        else:
+            store.append(num)
+
+    return store
+
+
+print(flattenArray(arr))
