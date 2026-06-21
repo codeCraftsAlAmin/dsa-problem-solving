e8446fb8c746cfecd0de31dc84d36f333da02b88
Code Crafts <codecraftsman12@gmail.com>
2026-06-21 20:19:28 +0600

    feat: solved check-even_or_odd

---
+++ check_even_or_odd.py
@@ -0,0 +1,24 @@
+# ============================================
+# Problem: Logic
+# Interview: #15
+# Difficulty: Easy
+# ============================================
+#
+# Problem Statement:
+# Check Even or Odd.
+# Input: 4, 7
+# Output: True, False
+#
+# Time Complexity:  O(1)
+# Space Complexity: O(1)
+# ============================================
+
+
+def isEven(n):
+
+    if n % 2 == 0:
+        return True
+    return False
+
+
+print(isEven(25))
